d7230b4e08396c85afdc528eefd57c9a3a370791
noah <noah201851@gmail.com>
2022-12-19 03:02:44 +0900

    ミント価格の16進数そのまま表示 をもとに戻す

--- contexts/NftContractProvider.tsx
+++ contexts/NftContractProvider.tsx
@@ -72,7 +72,7 @@ const Component: React.FC<Props> = ({ children }: Props) => {
     })
 
     nftDrop?.claimConditions.getActive().then((activeClaimCondition) => {
-      setClaimPrice(String(activeClaimCondition.price))
+      setClaimPrice(ethers.utils.formatUnits(activeClaimCondition.price._hex))
     })
   }, [nftDrop])
 
@@ -105,7 +105,7 @@ const Component: React.FC<Props> = ({ children }: Props) => {
     })
 
     nftDrop?.claimConditions.getActive().then((activeClaimCondition) => {
-      setClaimPrice(String(activeClaimCondition.price))
+      setClaimPrice(ethers.utils.formatUnits(activeClaimCondition.price._hex))
     })
   }, [isClaiming])
 
